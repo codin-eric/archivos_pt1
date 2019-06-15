f658131376cd88362add2fd5a9b37687a7627468
risheric <risheric@gmail.com>
2019-06-15 18:29:28 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Coding-eric video 14
+Archivos
+'''
+
+datos = "hola mundo 2"
+
+# file = open("datos.txt", "w")
+# file.write(datos)
+# file.close()
+
+with open("datos.txt", "w") as file:
+	file.write(datos)
+
+with open("datos.txt", "r") as file:
+	text = file.read()
+
+print(text)
